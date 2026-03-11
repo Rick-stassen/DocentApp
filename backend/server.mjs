4f2357e6699ea882c00a563cccfce926e602afd3
monkey-and-me <526275@vistacollege.nl>
2026-03-11 12:57:39 +0100

    de port is url in command promt

--- backend/server.mjs
+++ backend/server.mjs
@@ -19,4 +19,4 @@ app.get('/', (req, res) => {
   res.send('Backend is running');
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log("http://localhost:3000"));

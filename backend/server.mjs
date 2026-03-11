dd2d19dfbd3dfd0dbe66c44a2c6a956b97391e1f
monkey-and-me <526275@vistacollege.nl>
2026-03-11 12:53:55 +0100

    back-end
    backend is in de backend file en dan de server.mjs

---
+++ backend/server.mjs
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let messages = [];
+
+app.get('/messages', (req, res) => {
+  res.json(messages);
+});
+
+app.post('/messages', (req, res) => {
+  messages.push(req.body);
+  res.json({ status: 'success' });
+});
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
